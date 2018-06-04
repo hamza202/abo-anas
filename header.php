276fb14f85695e7278ff5224f5f2314761cadc77
The End 7moz <k8668975@gmail.com>
2018-06-04 23:39:23 +0300

    First commit

---
+++ header.php
@@ -0,0 +1,42 @@
+<header class="main-navigation" id="sticker" >
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <nav class="navbar">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#box-nav" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="white navbar-brand" href="#"><img src="img/logow.png" alt=""></a>
+                        <a class="dark navbar-brand" href="#"><img src="img/logo.png" alt=""></a>
+                    </div>
+                    <div class="collapse navbar-collapse" id="box-nav">
+                        <ul id="nav" class="nav navbar-nav navbar-right">
+                            <li class="current"><a href="#slider">home</a>
+
+                            </li>
+                            <li><a href="#about">about us</a></li>
+
+                            <li><a href="#works">PRODUCTS</a></li>
+
+
+                            <li><a href="#contact">contact</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Language
+                                    <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">العربية</a></li>
+                                    <li><a href="#">English</a></li>
+                                    <li><a href="#">Türkçe</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
